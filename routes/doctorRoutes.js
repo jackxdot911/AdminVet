a66b8cd5b4f98a9004f2d3a6e1c7141d42d3ba6c
nishan666 <nishankumar559@gmail.com>
2024-04-17 09:45:26 +0530

    working  /alive route

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const doctorController = require('../controllers/doctorController');
 
+router.get('/alive',doctorController.keepAlive);
+
 router.get('/', doctorController.getAllDoctors);
 router.post('/', doctorController.createDoctor);
 router.delete('/:id', doctorController.deleteDoctor);
 router.put('/:id', doctorController.updateDoctor);
 
 router.get('/:id', doctorController.getDoctorById);
-
-router.get('/alive',doctorController.keepAlive);
 
 module.exports = router;
